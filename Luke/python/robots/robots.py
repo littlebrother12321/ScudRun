08515eb701e597aae3bc052f0d5e442674d3df76
Luke <eldredgething4@gmail.com>
2019-03-23 20:24:05 -0600

    I made junk into fire now

--- Luke/python/robots/robots.py
+++ Luke/python/robots/robots.py
@@ -31,6 +31,7 @@ if(pygame.joystick.get_count()):
 #load robot pictures
 greenbot = pygame.image.load('greenbot.png');
 playerimg = pygame.image.load('player.png');
+junkimg = pygame.image.load('junk.png');
  #game states
 keepPlaying = True;
 
@@ -79,7 +80,8 @@ class Lockbot(mob):
    def render(self):
       # .. draw a robot
       if self.broken:
-         pygame.draw.rect(screen, (0,0,0),(self.pos_x-5,self.pos_y-5,10,10), 0);
+         #pygame.draw.rect(screen, (0,0,0),(self.pos_x-5,self.pos_y-5,10,10), 0);
+         screen.blit(junkimg, [self.pos_x-5, self.pos_y-5]);
       else:
          #pygame.draw.rect(screen, (255,0,0),(self.pos_x-5,self.pos_y-5,10,10), 0);
          screen.blit(greenbot, [self.pos_x-5, self.pos_y-5]);
@@ -113,7 +115,8 @@ class Wildbot(mob):
    def render(self):
       # .. draw a robot
       if self.broken:
-         pygame.draw.rect(screen, (0,0,0),(self.pos_x-5,self.pos_y-5,10,10), 0);
+         #pygame.draw.rect(screen, (0,0,0),(self.pos_x-5,self.pos_y-5,10,10), 0);
+         screen.blit(junkimg, [self.pos_x-5, self.pos_y-5]);
       else:
          pygame.draw.rect(screen, (128,0,0),(self.pos_x-5,self.pos_y-5,10,10), 0);
    def update(self,x,y):
