9fef7f5c79ed975f7d0ed2f4eac54235528df5d1
RawSteak0 <eldredgething3@gmail.com>
2019-03-24 18:02:24 -0600

    this is new thing

---
+++ James/python/number_guess.py
@@ -0,0 +1,14 @@
+import random;
+max_number = 100;
+random.seed();
+print "guesss a number between 1 and " + str(max_number); 
+
+secret_number = random.randint(1,max_number);
+
+print "it is " + str(secret_number);
+
+guess = 0;
+ 
+while guess != secret_number:
+   guess=input("guess again");
+   print "you guessed " + str(guess);
