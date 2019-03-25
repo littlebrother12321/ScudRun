727f379cd6e724fd3bb82545e9c54705ece867b1
RawSteak0 <eldredgething3@gmail.com>
2019-03-24 18:10:51 -0600

    git rid of it telling you

--- James/python/number_guess.py
+++ James/python/number_guess.py
@@ -5,10 +5,12 @@ print "guesss a number between 1 and " + str(max_number);
 
 secret_number = random.randint(1,max_number);
 
-print "it is " + str(secret_number);
+
 
 guess = 0;
  
 while guess != secret_number:
    guess=input("guess again");
    print "you guessed " + str(guess);
+
+print "you got it"
