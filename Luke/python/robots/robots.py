782eac35e1d7d22414dd9eb16532faf41b28190c
Luke <eldredgething4@gmail.com>
2019-06-02 20:10:25 -0600

    the end game thing is complete

--- Luke/python/robots/robots.py
+++ Luke/python/robots/robots.py
@@ -403,6 +403,8 @@ def quit_menu():
    global screen_text;
    KeepPlayingText = scorefont.render("Do you want to play again?", 1, (255,255,255));
    screen.blit(KeepPlayingText, [400,415]);
+   KeepPlayingText = scorefont.render("(if yes press y if no press n)", 1, (255,255,255));
+   screen.blit(KeepPlayingText, [400,430]);
 #   screen_text = "do you want to play again?";
    pygame.display.flip();
    pygame.time.wait(1000);
