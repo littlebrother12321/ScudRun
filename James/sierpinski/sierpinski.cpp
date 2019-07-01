d4f4e799b154f82e99955ee94a4ec1920b0fe9fd
Elnathan James Eldrege <rawbeef@greybeard.echo.net>
2019-06-30 18:13:38 -0600

    there is a triangle

--- James/sierpinski/sierpinski.cpp
+++ James/sierpinski/sierpinski.cpp
@@ -12,6 +12,17 @@ void drawDot()
    glClearColor(0.4, 0.4, 0.4, 0.4);
    glClear(GL_COLOR_BUFFER_BIT);
 
+   glColor3f(1.0, 1.0, 0.5);
+   glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+  // gl_PointSize = 2.0;
+  // for this to work we need an array of points
+  // glDrawArrays(GL_POINTS, 0, 3)
+
+   glBegin(GL_LINE_LOOP);
+      glVertex3f(-1.0, -1.0, 0.0);
+      glVertex3f(0.0, 0.5, 0.0);
+      glVertex3f(1.0, -1.0, 0.0);
+   glEnd();
 
    glFlush();
 
